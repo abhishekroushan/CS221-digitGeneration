253f25be3952fc5802eef1ae8a8f03e0090be282
Clara Mc <chmccreery@gmail.com>
2018-11-30 00:42:54 -0800

    add script to manually create 2

---
+++ write_2.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.image import imsave
+
+def write_2():
+    expected_img = np.zeros((28,28))
+    expected_img[6:9,6:18] = 1
+    expected_img[9:11,6:10] = 1
+    expected_img[9:12,15:20] = 1
+    expected_img[12:20,17:21] = 1
+    expected_img[18:22,6:18] = 1
+    expected_img[20:23,16:24] = 1
+    expected_img[20:23,6:10] = 1
+    imsave('custom2.png',expected_img)
+    plt.imshow(expected_img)
+    plt.pause(5) 
+
+write_2()
