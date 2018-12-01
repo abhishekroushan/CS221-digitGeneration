51340f0dd82a2b7149c297aebe40cba47f511674
Clara Mc <chmccreery@gmail.com>
2018-12-01 11:10:35 -0800

    full runs implemented

--- rl_solver2.py
+++ rl_solver2.py
@@ -1,9 +1,11 @@
 from keras.models import model_from_json
+import random
 import numpy as np
 import matplotlib.image as mpimg
 import pickle
 import matplotlib.pyplot as plt
 
+# Loads MNIST-trained CNN to classify digit in a (28,28) canvas 
 def load_json():
 	json_file = open('model.json','r')
 	loaded_model_json = json_file.read()
@@ -13,6 +15,7 @@ def load_json():
 	print("Loaded model from disk")
 	return loaded_model	
 
+# Uses MNIST-trained CNN to classify digit in a (28,28) canvas and return cost relative to its similarity to a 2
 def evaluate(img):
 	img = np.expand_dims(img,axis=0)
 	img = np.expand_dims(img,axis=3)
@@ -22,22 +25,24 @@ def evaluate(img):
 	cost = -100*(prob2-0.5)	
 	return cost
 
+# Test the image that Clara custom-made
 def test_oracle():
 	img = pickle.load(open('custom2.pkl','rb'))
 	print(img.shape)
 	cost = evaluate(img)
 	print(cost)
 
+
 class RLmodel(object):
 	def __init__(self,pen,N):
-		self.startpen=pen                #(x,y) tuple robot start state
-		self.N=N                    #canvas size
-		self.end=N*3/4.0
-		self.p_inde = {}
-		for x in range(0,N):
+		self.startpen=pen          #(x,y) tuple robot start state
+		self.N=N                   #canvas size
+		self.end=N*3/4.0	   #threshold for lower-right-hand part of canvas
+		self.p_inde = {}	   #probabilities of each action independent of previous actions
+		for x in range(0,N):	   #initially all probabilities are equal
 			for y in range(0,N):
 				self.p_inde[(x,y)] = (0.25, 0.25, 0.25, 0.25)
-		self.actions=["r","l","u","d"]
+		self.actions=["r","l","u","d"] #possible actions from any state
 
 	def startState(self):
         	#state=((x,y),prev,prev_prev)
@@ -50,22 +55,36 @@ class RLmodel(object):
 		return ((curr_coords[0]>=self.end) and (curr_coords[1]>=self.end))
 
 	def getActions(self,state):
+		# handle edge cases in isEnd
 		return self.actions
 
+	def getSuccessor(self,state,a):
+		pen, prev, prev_prev = state
+		if a == "r": new_pen = (pen[0],pen[1]+1)
+		elif a == "l": new_pen = (pen[0],pen[1]-1)
+		elif a == "d": new_pen = (pen[0]+1,pen[1])
+		elif a == "u": new_pen = (pen[0]-1,pen[1])
+		else: print("Invalid action")
+		return (new_pen,a,prev)
+
 	def getIndex(self,action):
+		# get index of a given action in self.actions
 		for i in range(0,4):
 			if action == self.actions[i]: return i
 		return -1
 
 	def getAdjustedProbs(self,state):
-		prev = getIndex(state[1])
-		prev_prev = getIndex(state[2])
+		# get adjusted probabilities, using momentum of previous two strokes to increase their probabilities
+		p_inde = self.p_inde[state[0]]
+		prev = self.getIndex(state[1])
+		prev_prev = self.getIndex(state[2])
 		eta = 0.5
 		aug = np.zeros(4)
 		for i in range(0,4):
 			aug[i] = eta*(prev == i)+eta*(prev_prev == i)
-		augtot = np.sum(aug)
-		p = ((self.p_inde[0]+aug[0])/augtot, (self.p_inde[1]+aug[1])/augtot,(self.p_inde[2]+aug[2])/augtot, (self.p_inde[3]+aug[3])/augtot)
+		augtot = np.sum(aug)+1
+		pr = (p_inde[0]+aug[0])/augtot
+		p = ((p_inde[0]+aug[0])/augtot, (p_inde[1]+aug[1])/augtot,(p_inde[2]+aug[2])/augtot, (p_inde[3]+aug[3])/augtot)
 		return p
 
 	def makeCanvas(self,actions):
@@ -79,14 +98,44 @@ class RLmodel(object):
 			elif a == "u": pen[0] -= 1
 			else: print("Invalid action in actions")
 		plt.imshow(canvas)
-		plt.pause(4)	
+		plt.pause(1)	
 		evaluate(canvas)
 
+def nextAction(probs,actions):
+	rand = random.random()
+	print(rand)
+	if rand<probs[0]: return actions[0]
+	elif rand<probs[0]+probs[1]: return actions[1]
+	elif rand<probs[0]+probs[1]+probs[2]: return actions[2]
+	else: return actions[3]
+
+# TESTING EACH METHOD
+# print(model.getSuccessor(state,'u'))
+# print("Start state: {}".format(ourmodel.startState()))
+# print("Should be end: {}".format(ourmodel.isEnd(((22,25),'right','right'))))
+# print("Should not be end: {}".format(ourmodel.isEnd(ourmodel.startState())))
+# print("Adjusted probs after moving right, down: {}".format(ourmodel.getAdjustedProbs(((7,9),'r','r'))))
+# ourmodel.makeCanvas(['r','r','r','d','d','d'])
+
 loaded_model = load_json()
-ourmodel = RLmodel((7,7),28)
-print("Start state: {}".format(ourmodel.startState()))
-print("Should be end: {}".format(ourmodel.isEnd(((22,25),'right','right'))))
-print("Should not be end: {}".format(ourmodel.isEnd(ourmodel.startState())))
-ourmodel.makeCanvas(['r','r','r','d','d','d'])
+model = RLmodel((7,7),28)
+
+for iter in range(0,10):
+	state = model.startState()
+	num_moves = 0
+	actions = []
+	print(actions)
+	print(state)
+	while(num_moves < 100):
+		num_moves += 1
+		probs = model.getAdjustedProbs(state)
+		possible_actions = model.getActions(state)
+		next_action = nextAction(probs,possible_actions)
+		state = model.getSuccessor(state,next_action) 
+		if(model.isEnd(state)): break
+		actions.append(next_action)
+	model.makeCanvas(actions)
+	# implement learning here
+
 
 
