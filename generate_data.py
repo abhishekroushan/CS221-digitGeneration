off by one bug fixed and analysis, baseline implemented

generate_data.py
@@ -50,28 +50,28 @@ for n in range(0,n_examples):
         probs = getNextActionProbabilities(prev,prev2,prev3)
         action = nextAction(probs,pen)
         #print("action: {}".format(action))
-        if action == 0: pen[1] += 1 #right
-        elif action == 1: pen[1] -= 1 #left
-        elif action == 2: pen[0] -= 1 #up
-        else: pen[0] += 1 #down
-        canvas[pen[0],pen[1]] = 1
         idx = n*max_moves+num_moves
         if((idx+1) % 10000 == 0):
             print('{} out of {} complete'.format(idx+1,70000))
         x[idx,:,:,1]=canvas
         x[idx,pen[0],pen[1],2]=1
         y[idx]=action
+        if action == 0: pen[1] += 1 #right
+        elif action == 1: pen[1] -= 1 #left
+        elif action == 2: pen[0] -= 1 #up
+        else: pen[0] += 1 #down
+        canvas[pen[0],pen[1]] = 1
         prev3 = prev2
         prev2 = prev
         prev = action
         num_moves += 1
     
     x[n*num_moves:(n+1)*num_moves,:,:,0]=canvas
-plt.imshow(x[10,:,:,0])
+plt.imshow(x[50,:,:,0])
 plt.pause(2)
-plt.imshow(x[10,:,:,1])
+plt.imshow(x[50,:,:,1])
 plt.pause(2)
-plt.imshow(x[10,:,:,2])
+plt.imshow(x[50,:,:,2])
 plt.pause(2)
 #random split
 x_train=np.array(x[0:60000, :,:,:])

train_model.py
@@ -7,6 +7,7 @@ Gets to 99.25% test accuracy after 12 epochs
 
 from __future__ import print_function
 import keras
+from keras.models import model_from_json
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
@@ -18,7 +19,7 @@ import matplotlib.pyplot as plt
 
 batch_size = 1
 num_classes = 4
-epochs = 1
+epochs = 20
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -30,10 +31,8 @@ def load_data():
     y_test=np.load('y_test.npy')
     return x_train, y_train, x_test, y_test
 
-# the data, split between train and test sets
+# get and format the data, split between train and test sets
 x_train, y_train, x_test, y_test=load_data()
-#(x_train, y_train), (x_test, y_test) = supervised_generator.load_data()
-
 if K.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 3, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 3, img_rows, img_cols)
@@ -42,7 +41,6 @@ else:
     x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 3)
     x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 3)
     input_shape = (img_rows, img_cols, 3)
-
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 print('x_train shape:', x_train.shape)
@@ -72,29 +70,34 @@ def action_CNN(input_shape, nclasses, data_format, act='relu', init='glorot_unif
   return Model(inputs=input_img,outputs=probs, name='action_model')
 
 #test summary model
-#xtrain=np.random.rand(100,28,28,3)
-#ytrain=np.random.rand(100,num_classes)
-#ytrain[ytrain>=0.5]=1; ytrain[ytrain<0.5]=0
-#xtest=np.random.rand(100,28,28,3)
-#ytest=np.random.rand(100,num_classes)
-#ytest[ytest>=0.5]=1; ytest[ytest<0.5]=0 
-xtrain, ytrain, xtest, ytest=x_train, y_train, x_test, y_test
-my_model=action_CNN((28,28,3), num_classes, 'channels_last')
-my_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
-my_model.summary()
+def create_model():
+    xtrain, ytrain, xtest, ytest=x_train, y_train, x_test, y_test
+    my_model=action_CNN((28,28,3), num_classes, 'channels_last')
+    my_model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adadelta(), metrics=['accuracy'])
+    my_model.summary()
+    my_model.fit(xtrain, ytrain, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(xtest, ytest))
+    score=my_model.evaluate(xtest,ytest, verbose=0)
+    model_json = my_model.to_json()
+    with open("CNN_model_20.json","w") as json_file:
+        json_file.write(model_json)
+    my_model.save_weights("CNN_model_20.h5")
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    return my_model
 
-#fit model
-my_model.fit(xtrain, ytrain, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(xtest, ytest))
-score=my_model.evaluate(xtest,ytest, verbose=0)
-model_json = my_model.to_json()
-with open("CNN_model.json","w") as json_file:
-  json_file.write(model_json)
-my_model.save_weights("CNN_model.h5")
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+def get_model():
+    with open("CNN_model.json","r") as json_file:
+        loaded_model_json = json_file.read()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("CNN_model.h5")
+    print("Loaded model architecture and weights")
+    return loaded_model
+
+my_model = create_model()
+my_model = get_model()
 
 #visualize
-for i in range(0,70):
+for i in range(0,7000):
     x_example = np.zeros((1,28,28,3))
     x_example[0,:,:,:] = x_train[i,:,:,:]
     print(type(x_example))
@@ -103,33 +106,5 @@ for i in range(0,70):
     y_true = y_train[i]
     print('True: {}'.format(np.argmax(y_true)))
     plt.imshow(x_train[i,:,:,:])
-    plt.pause(3)
+    plt.pause(1)
 
-#model = Sequential()
-#model.add(Conv2D(32, kernel_size=(3, 3),
-#                 activation='relu',
-#                 input_shape=input_shape))
-#model.add(Conv2D(64, (3, 3), activation='relu'))
-#model.add(MaxPooling2D(pool_size=(2, 2)))
-#model.add(Dropout(0.25))
-#model.add(Flatten())
-#model.add(Dense(128, activation='relu'))
-#model.add(Dropout(0.5))
-#model.add(Dense(num_classes, activation='softmax'))
-
-#model.compile(loss=keras.losses.categorical_crossentropy,
-#              optimizer=keras.optimizers.Adadelta(),
-#              metrics=['accuracy'])
-
-#model.fit(x_train, y_train,
-#          batch_size=batch_size,
-#          epochs=epochs,
-#          verbose=1,
-#          validation_data=(x_test, y_test))
-#score = model.evaluate(x_test, y_test, verbose=0)
-#model_json = model.to_json()
-#with open("CNN_model.json","w") as json_file:
-#	json_file.write(model_json)
-#model.save_weights("CNN_model.h5")
-#print('Test loss:', score[0])
-#print('Test accuracy:', score[1])